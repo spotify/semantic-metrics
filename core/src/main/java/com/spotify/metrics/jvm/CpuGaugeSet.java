1d6dc9cda58caa088426d75ab1c259d1955f2888
John-John Tedro <udoprog@spotify.com>
2016-06-14 16:25:01 +0200

    [core] add license and fix indentation for CpuGaugeSet

--- core/src/main/java/com/spotify/metrics/jvm/CpuGaugeSet.java
+++ core/src/main/java/com/spotify/metrics/jvm/CpuGaugeSet.java
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2016 Spotify AB.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package com.spotify.metrics.jvm;
 
 import com.codahale.metrics.Gauge;
@@ -16,55 +37,58 @@ import java.util.Map;
  */
 public class CpuGaugeSet implements SemanticMetricSet {
 
-  private final OperatingSystemMXBean operatingSystemMXBean;
+    private final OperatingSystemMXBean operatingSystemMXBean;
 
-  private CpuGaugeSet(OperatingSystemMXBean operatingSystemMXBean) {
-    this.operatingSystemMXBean = operatingSystemMXBean;
-  }
-
-  @Override
-  public Map<MetricId, Metric> getMetrics() {
-    if (!(operatingSystemMXBean instanceof com.sun.management.OperatingSystemMXBean)) {
-      return Collections.emptyMap();
+    private CpuGaugeSet(OperatingSystemMXBean operatingSystemMXBean) {
+        this.operatingSystemMXBean = operatingSystemMXBean;
     }
 
-    final com.sun.management.OperatingSystemMXBean osMxBean = (com.sun.management.OperatingSystemMXBean) operatingSystemMXBean;
+    @Override
+    public Map<MetricId, Metric> getMetrics() {
+        if (!(operatingSystemMXBean instanceof com.sun.management.OperatingSystemMXBean)) {
+            return Collections.emptyMap();
+        }
 
-    final Map<MetricId, Metric> gauges = new HashMap<>();
-    final MetricId cpu = MetricId.build().tagged("what", "jvm-cpu-stats");
+        final com.sun.management.OperatingSystemMXBean osMxBean =
+            (com.sun.management.OperatingSystemMXBean) operatingSystemMXBean;
 
-    gauges.put(cpu.tagged("what", "process-cpu-load-percentage", "unit", "%"), new Gauge<Double>() {
-      @Override
-      public Double getValue() {
-        return osMxBean.getProcessCpuLoad();
-      }
-    });
+        final Map<MetricId, Metric> gauges = new HashMap<>();
+        final MetricId cpu = MetricId.build().tagged("what", "jvm-cpu-stats");
 
-    gauges.put(cpu.tagged("what", "system-cpu-load-percentage", "unit", "%"), new Gauge<Double>() {
-      @Override
-      public Double getValue() {
-        return osMxBean.getSystemCpuLoad();
-      }
-    });
+        gauges.put(cpu.tagged("what", "process-cpu-load-percentage", "unit", "%"),
+            new Gauge<Double>() {
+                @Override
+                public Double getValue() {
+                    return osMxBean.getProcessCpuLoad();
+                }
+            });
 
-    gauges.put(cpu.tagged("what", "system-load-average"), new Gauge<Double>() {
-      @Override
-      public Double getValue() {
-        return osMxBean.getSystemLoadAverage();
-      }
-    });
+        gauges.put(cpu.tagged("what", "system-cpu-load-percentage", "unit", "%"),
+            new Gauge<Double>() {
+                @Override
+                public Double getValue() {
+                    return osMxBean.getSystemCpuLoad();
+                }
+            });
 
-    gauges.put(cpu.tagged("what", "process-cpu-time", "unit", "ns"), new Gauge<Long>() {
-      @Override
-      public Long getValue() {
-        return osMxBean.getProcessCpuTime();
-      }
-    });
+        gauges.put(cpu.tagged("what", "system-load-average"), new Gauge<Double>() {
+            @Override
+            public Double getValue() {
+                return osMxBean.getSystemLoadAverage();
+            }
+        });
 
-    return Collections.unmodifiableMap(gauges);
-  }
+        gauges.put(cpu.tagged("what", "process-cpu-time", "unit", "ns"), new Gauge<Long>() {
+            @Override
+            public Long getValue() {
+                return osMxBean.getProcessCpuTime();
+            }
+        });
 
-  public static CpuGaugeSet create() {
-    return new CpuGaugeSet(ManagementFactory.getOperatingSystemMXBean());
-  }
+        return Collections.unmodifiableMap(gauges);
+    }
+
+    public static CpuGaugeSet create() {
+        return new CpuGaugeSet(ManagementFactory.getOperatingSystemMXBean());
+    }
 }

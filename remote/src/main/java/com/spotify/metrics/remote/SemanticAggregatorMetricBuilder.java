e3a604c3ec7b9d32ba00836dc1f9e5d03d2f8189
Alexander Dergachev <alexander@spotify.com>
2017-01-04 10:45:40 +0100

    Add remote counters support

--- remote/src/main/java/com/spotify/metrics/remote/SemanticAggregatorMetricBuilder.java
+++ remote/src/main/java/com/spotify/metrics/remote/SemanticAggregatorMetricBuilder.java
@@ -23,6 +23,7 @@ package com.spotify.metrics.remote;
 
 import com.google.common.util.concurrent.ListenableFuture;
 
+import com.spotify.metrics.core.RemoteCounter;
 import com.spotify.metrics.core.RemoteTimer;
 import com.spotify.metrics.core.RemoteHistogram;
 import com.spotify.metrics.core.RemoteMeter;
@@ -75,6 +76,55 @@ public interface SemanticAggregatorMetricBuilder<T extends RemoteMetric> {
             @Override
             public boolean isInstance(final RemoteMetric metric) {
                 return RemoteMeter.class.isInstance(metric);
+            }
+        };
+
+    SemanticAggregatorMetricBuilder<RemoteCounter> REMOTE_COUNTERS =
+        new SemanticAggregatorMetricBuilder<RemoteCounter>() {
+            @Override
+            public RemoteCounter newMetric(
+                final MetricId id,
+                final List<String> shardKey,
+                final Remote remote) {
+
+                final Map<String, String> allAttributes =
+                    SemanticAggregator.buildAttributes(id, "counter");
+                final String shard =
+                    Sharder.buildShardKey(shardKey, allAttributes);
+
+                return new RemoteCounter() {
+                    @Override
+                    public void inc() {
+                        inc(1);
+                    }
+
+                    @Override
+                    public void inc(long n) {
+                        remote.post(
+                            "/",
+                            shard,
+                            SemanticAggregator.buildDocument(
+                                Long.toString(n),
+                                id.getKey(),
+                                allAttributes));
+                    }
+
+                    @Override
+                    public void dec() {
+                        inc(-1);
+                    }
+
+                    @Override
+                    public void dec(long n) {
+                        inc(-n);
+                    }
+
+                };
+            }
+
+            @Override
+            public boolean isInstance(final RemoteMetric metric) {
+                return RemoteCounter.class.isInstance(metric);
             }
         };
 

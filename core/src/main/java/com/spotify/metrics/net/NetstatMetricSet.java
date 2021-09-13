ac13d05e6b088269dbbcd2c5f1d9f7ae29727058
Tommy Ulfsparre <tommy@spotify.com>
2021-09-13 12:18:48 +0200

    wip: add a metricset for collecting metrics mostly related to TCP paket-loss and friends

---
+++ core/src/main/java/com/spotify/metrics/net/NetstatMetricSet.java
@@ -0,0 +1,129 @@
+package com.spotify.metrics.net;
+
+import com.codahale.metrics.Metric;
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Supplier;
+import com.google.common.base.Suppliers;
+import com.spotify.metrics.core.DerivedLongGauge;
+import com.spotify.metrics.core.MetricId;
+import com.spotify.metrics.core.SemanticMetricSet;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class NetstatMetricSet implements SemanticMetricSet {
+
+    private static final String SPACES = "\\s+";
+    private static final String PROC_SNMP_PATH = "/proc/net/snmp";
+    private static final String PROC_NETSTAT_PATH = "/proc/net/netstat";
+
+    private final Supplier<Data> dataSupplier;
+
+    public NetstatMetricSet() {
+        this(Suppliers.memoizeWithExpiration(NetstatMetricSet::readData, 10, TimeUnit.SECONDS));
+    }
+
+    @VisibleForTesting
+    NetstatMetricSet(final Supplier<Data> dataSupplier) {
+        this.dataSupplier = dataSupplier;
+    }
+
+    @Override
+    public Map<MetricId, Metric> getMetrics() {
+        final Map<MetricId, Metric> gauges = new HashMap<>();
+        gauges.put(
+            MetricId.build().tagged("what", "segments-retransmitted", "unit", "tcp-segments/s"),
+            new DerivedLongGauge() {
+                @Override
+                public Long getNext() {
+                    return dataSupplier.get().segmentsRetransmitted;
+                }
+            });
+        gauges.put(
+            MetricId.build().tagged("what", "bad-segments-received", "unit", "tcp-segments/s"),
+            new DerivedLongGauge() {
+                @Override
+                public Long getNext() {
+                    return dataSupplier.get().badSegmentsReceived;
+                }
+            });
+        gauges.put(MetricId.build().tagged("what", "tcp-timeouts", "unit", "timeouts/s"),
+            new DerivedLongGauge() {
+                @Override
+                public Long getNext() {
+                    return dataSupplier.get().tcpTimeouts;
+                }
+            });
+        gauges.put(MetricId.build().tagged("what", "tcp-lost-retransmit", "unit", "retransmit/s"),
+            new DerivedLongGauge() {
+                @Override
+                public Long getNext() {
+                    return dataSupplier.get().tcpLostRetransmit;
+                }
+            });
+        gauges.put(MetricId.build().tagged("what", "tcp-syn-retrans", "unit", "retransmit/s"),
+            new DerivedLongGauge() {
+                @Override
+                public Long getNext() {
+                    return dataSupplier.get().tcpSynRetrans;
+                }
+            });
+
+        return gauges;
+    }
+
+    private static Data readData() {
+        return readData(PROC_NETSTAT_PATH, PROC_SNMP_PATH);
+    }
+
+    @VisibleForTesting
+    static Data readData(final String netstatPath, final String snmpPath) {
+        final Data data = new Data();
+        final Map<String, Long> netstatValues = readFile(netstatPath, "TcpExt:");
+        final Map<String, Long> snmpValues = readFile(snmpPath, "Tcp:");
+
+        data.badSegmentsReceived = snmpValues.getOrDefault("InErrs", 0L);
+        data.segmentsRetransmitted = snmpValues.getOrDefault("RetransSegs", 0L);
+        data.tcpLostRetransmit = netstatValues.getOrDefault("TCPLostRetransmit", 0L);
+        data.tcpSynRetrans = netstatValues.getOrDefault("TCPSynRetrans", 0L);
+        data.tcpTimeouts = netstatValues.getOrDefault("TCPTimeouts", 0L);
+
+        return data;
+    }
+
+    private static Map<String, Long> readFile(final String fileName, final String prefix) {
+        final Map<String, Long> kv = new HashMap<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] keys = line.trim().split(SPACES);
+                line = br.readLine();
+                String[] values = line.trim().split(SPACES);
+                if (keys.length == values.length && keys.length > 0 &&
+                    prefix.equals(keys[0].trim()) && prefix.equals(values[0].trim())) {
+                    for (int i = 1; i < keys.length; i++) {
+                        long value = Long.parseLong(values[i]);
+                        kv.put(keys[i], value);
+                    }
+                }
+            }
+        } catch (IOException ex) {
+            return Collections.emptyMap();
+        }
+
+        return Collections.unmodifiableMap(kv);
+    }
+
+    static class Data {
+        private long segmentsRetransmitted;
+        private long badSegmentsReceived;
+        private long tcpTimeouts;
+        private long tcpLostRetransmit;
+        private long tcpSynRetrans;
+    }
+}
---
+++ core/src/test/java/com/spotify/metrics/net/NetstatMetricSetTest.java
@@ -0,0 +1,103 @@
+package com.spotify.metrics.net;
+
+import static org.junit.Assert.assertEquals;
+
+import com.codahale.metrics.Gauge;
+import com.google.common.base.Supplier;
+import com.google.common.base.Suppliers;
+import com.spotify.metrics.core.DerivedLongGauge;
+import com.spotify.metrics.core.MetricId;
+import com.spotify.metrics.core.SemanticMetricFilter;
+import com.spotify.metrics.core.SemanticMetricRegistry;
+import java.io.File;
+import java.util.Optional;
+import java.util.SortedMap;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class NetstatMetricSetTest {
+
+    private SemanticMetricRegistry registry;
+
+    @Before
+    public void setUp() {
+        registry = new SemanticMetricRegistry();
+    }
+
+    @Test
+    public void testParseProc() {
+        NetstatMetricSet netstatMetricSet = new NetstatMetricSet(
+            () -> NetstatMetricSet.readData(absolutePath("proc/net/netstat"),
+                absolutePath("proc/net/snmp")));
+
+        registry.registerAll(netstatMetricSet);
+        assertEquals(15170L, valueFrom(registry.getGauges(filter("segments-retransmitted"))));
+        assertEquals(0L, valueFrom(registry.getGauges(filter("bad-segments-received"))));
+        assertEquals(2677L, valueFrom(registry.getGauges(filter("tcp-timeouts"))));
+        assertEquals(2055L, valueFrom(registry.getGauges(filter("tcp-lost-retransmit"))));
+        assertEquals(0L, valueFrom(registry.getGauges(filter("tcp-syn-retrans"))));
+    }
+
+    @Test
+    public void testHandleFileNotFound() {
+        NetstatMetricSet netstatMetricSet = new NetstatMetricSet(
+            () -> NetstatMetricSet.readData("/tmp/NetstatMetricSetTest.testHandleFileNotFound",
+                "/tmp/NetstatMetricSetTest.testHandleFileNotFound"));
+        registry.registerAll(netstatMetricSet);
+        assertEquals(0L, valueFrom(registry.getGauges(filter("segments-retransmitted"))));
+        assertEquals(0L, valueFrom(registry.getGauges(filter("bad-segments-received"))));
+        assertEquals(0L, valueFrom(registry.getGauges(filter("tcp-timeouts"))));
+        assertEquals(0L, valueFrom(registry.getGauges(filter("tcp-lost-retransmit"))));
+        assertEquals(0L, valueFrom(registry.getGauges(filter("tcp-syn-retrans"))));
+    }
+
+    @Test
+    public void testMalformedNetstatFile() {
+        NetstatMetricSet netstatMetricSet = new NetstatMetricSet(
+            () -> NetstatMetricSet.readData(absolutePath("proc/net/netstat.invalid"),
+                absolutePath("proc/net/snmp")));
+
+        registry.registerAll(netstatMetricSet);
+        assertEquals(15170L, valueFrom(registry.getGauges(filter("segments-retransmitted"))));
+        assertEquals(0L, valueFrom(registry.getGauges(filter("bad-segments-received"))));
+        assertEquals(0L, valueFrom(registry.getGauges(filter("tcp-timeouts"))));
+        assertEquals(0L, valueFrom(registry.getGauges(filter("tcp-lost-retransmit"))));
+        assertEquals(0L, valueFrom(registry.getGauges(filter("tcp-syn-retrans"))));
+    }
+
+    @Test
+    public void testMemoizingSupplier() {
+        AtomicInteger supplyCounter = new AtomicInteger();
+        Supplier<NetstatMetricSet.Data> dataSupplier = Suppliers.memoizeWithExpiration(() -> {
+            supplyCounter.incrementAndGet();
+            return new NetstatMetricSet.Data();
+        }, 10, TimeUnit.SECONDS);
+
+        NetstatMetricSet netstatMetricSet = new NetstatMetricSet(dataSupplier);
+
+        registry.registerAll(netstatMetricSet);
+        assertEquals(0L, valueFrom(registry.getGauges(filter("segments-retransmitted"))));
+        assertEquals(0L, valueFrom(registry.getGauges(filter("bad-segments-received"))));
+        assertEquals(1, supplyCounter.get());
+    }
+
+    private SemanticMetricFilter filter(final String what) {
+        return (metricId, metric) -> metricId.getTags().get("what").equals(what);
+    }
+
+    private long valueFrom(final SortedMap<MetricId, Gauge> map) {
+        Optional<Gauge> first = map.values().stream().findFirst();
+        DerivedLongGauge gauge = (DerivedLongGauge) first.get();
+        return gauge.getNext();
+    }
+
+    private String absolutePath(String name) {
+        File netStat = new File(getClass().getClassLoader().getResource(name).getFile());
+        return netStat.getAbsolutePath();
+    }
+}
